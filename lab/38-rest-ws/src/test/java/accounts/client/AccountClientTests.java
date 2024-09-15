3fb8a2018f45e5e0aae02fa79423e15419dcb6e1
tientu <tientu2003@gmail.com>
2024-09-16 04:14:58 +0700

    Temp

--- lab/38-rest-ws/src/test/java/accounts/client/AccountClientTests.java
+++ lab/38-rest-ws/src/test/java/accounts/client/AccountClientTests.java
@@ -22,15 +22,14 @@ public class AccountClientTests {
 	private Random random = new Random();
 	
 	@Test
-	@Disabled
 	public void listAccounts() {
 		// TODO-03: Run this test
 		// - Remove the @Disabled on this test method.
 		// - Then, use the restTemplate to retrieve an array containing all Account instances.
 		// - Use BASE_URL to help define the URL you need: BASE_URL + "/..."
 		// - Run the test and ensure that it passes.
-		Account[] accounts = null; // Modify this line to use the restTemplate
-		
+		Account[] accounts = restTemplate.getForObject(BASE_URL + "/accounts", Account[].class);
+
 		assertNotNull(accounts);
 		assertTrue(accounts.length >= 21);
 		assertEquals("Keith and Keri Donald", accounts[0].getName());
@@ -39,13 +38,12 @@ public class AccountClientTests {
 	}
 	
 	@Test
-	@Disabled
 	public void getAccount() {
 		// TODO-05: Run this test
 		// - Remove the @Disabled on this test method.
 		// - Then, use the restTemplate to retrieve the Account with id 0 using a URI template
 		// - Run the test and ensure that it passes.
-		Account account = null; // Modify this line to use the restTemplate
+		Account account = restTemplate.getForObject(BASE_URL + "/accounts/0",Account.class); // Modify this line to use the restTemplate
 		
 		assertNotNull(account);
 		assertEquals("Keith and Keri Donald", account.getName());
